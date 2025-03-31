5b35e0e5a30cd6e94305cb122b5bbd0f470f2712
pithambar20 <pithambar20@gmail.com>
2025-03-31 13:02:26 +0530

    second

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+let addList = () =>{
+
+    
+    let inputList = document.getElementById("input-list");   
+    let inputTime = document.getElementById("")
+
+
+    console.log(inputList.value);
+
+
+}
